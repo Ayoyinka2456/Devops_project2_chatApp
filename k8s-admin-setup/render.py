e637ecbb0d579058911d89b189a5de92c416c820
EC2 Default User <ec2-user@ip-172-31-47-107.us-east-2.compute.internal>
2025-06-06 12:53:32 +0000

    first push

---
+++ k8s-admin-setup/render.py
@@ -0,0 +1,26 @@
+from jinja2 import Environment, FileSystemLoader
+
+# Read the counter value from counter.txt
+with open('counter.txt') as f:
+    counter = f.read().strip()
+
+# Set up Jinja2 environment
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('project_deployment.j2')
+
+# Render the template
+output = template.render(counter=counter)
+
+# Write the rendered YAML to file
+with open('project_deployment.yml', 'w') as f:
+    f.write(output)
+
+
+# How to run j2 
+# python3 render.py 3
+# this creates the deployment.yml
+
+
+# RUN below command
+#kubectl apply -f project_deployment.yml
+
