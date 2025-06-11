3439b4387299595b6620c9a57b88c7b40911a635
Ayoyinka2456 <108306542+Ayoyinka2456@users.noreply.github.com>
2025-06-11 19:44:44 -0400

    Update deploy.sh

--- k8s-admin-setup/deploy.sh
+++ k8s-admin-setup/deploy.sh
@@ -1,25 +1,25 @@
-#!/bin/bash
+# #!/bin/bash
 
-cd /home/ec2-user/k8s-admin-setup/
+# cd /home/ec2-user/k8s-admin-setup/
 
-# Fail early if any required file is missing
-for f in TAG.txt K8S_IP.txt NGINX_IP.txt; do
-    [ ! -f "$f" ] && echo "❌ $f not found!" && exit 1
-done
+# # Fail early if any required file is missing
+# for f in TAG.txt K8S_IP.txt NGINX_IP.txt; do
+#     [ ! -f "$f" ] && echo "❌ $f not found!" && exit 1
+# done
 
-export TAG=$(cat TAG.txt | tr -d '[:space:]')
-export K8S_IP=$(cat K8S_IP.txt | tr -d '[:space:]')
-export NGINX_IP=$(cat NGINX_IP.txt | tr -d '[:space:]')
+# export TAG=$(cat TAG.txt | tr -d '[:space:]')
+# export K8S_IP=$(cat K8S_IP.txt | tr -d '[:space:]')
+# export NGINX_IP=$(cat NGINX_IP.txt | tr -d '[:space:]')
 
-echo "✅ TAG=$TAG"
-echo "✅ K8S_IP=$K8S_IP"
-echo "✅ NGINX_IP=$NGINX_IP"
+# echo "✅ TAG=$TAG"
+# echo "✅ K8S_IP=$K8S_IP"
+# echo "✅ NGINX_IP=$NGINX_IP"
 
-python3 render.py
-sudo chown ec2-user:ec2-user deployment.yml
-echo "Deploying pods"
-kubectl apply -f deployment.yml
-sleep 60
+# python3 render.py
+# sudo chown ec2-user:ec2-user deployment.yml
+# echo "Deploying pods"
+# kubectl apply -f deployment.yml
+# sleep 60
 
 
 # echo "Getting Service DNS and Port"
